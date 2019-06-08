749c7a16a953743397514d8f34e3f6a91370d38d
Xavier Mayeur <xavier@mayeur.be>
2019-06-08 23:33:16 +0200

    build for rpi

--- build-nodered.sh
+++ build-nodered.sh
@@ -1,8 +1,20 @@
-docker rm -f nodered
-docker build -t nodered .
+#!/bin/bash
+arch=`uname -a | grep arm`
+if [ -z "${arch}" ]
+then
+    sed -i '1s/docker.*$/docker:v8/' Dockerfile
+else
+    sed -i '1s/docker.*$/docker:rpi-v8/' Dockerfile
+fi
+
+docker rm -f nodered8
+docker build -t nodered8 .
+
+docker rm -f nodered8
+
 docker run -it -p 1880:1880 \
-    -v /home/xavier/Desktop/nodered/node-red-data:/data \
-    --name nodered \
+    -v /root/nodered/node-red-data:/data \
+    --name nodered8 \
     --dns 8.8.8.8 \
     --net host \
     nodered
